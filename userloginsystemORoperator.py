e82c7edf3f8d83b5f096491032d236b85abc11c4
Shreedhar <shreedhardahal2@gmail.com>
2020-06-06 18:32:19 +0530

    added user login system

--- userloginsystemORoperator.py
+++ userloginsystemORoperator.py
@@ -1,7 +1,7 @@
 username = "shreedhar"
 password = "shree123"
 
-if username == "shreedhar" and password == "shree123":
+if username == "shreedhar" or password == "shree123":
     print("Login Successful !")
 else:
     print("Not Logged in !")
